a50b5ccd91f93b87de357d19af1966ada5076037
Alex Skorulis <skorulis@gmail.com>
2026-02-27 16:52:47 +1100

    Get it running

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from "astro/config";
 // You will later set `site` to your actual GitHub Pages URL.
 
 export default defineConfig({
-  site: "https://<your-username>.github.io",
+  site: "https://skorulis.github.io",
   // If you use a project repo (e.g. ai.skorulis.com),
   // update base to `'/ai.skorulis.com'`.
   // base: "/ai.skorulis.com",
